26e454fe3d7319e6b7f5ea44855144e811f3af68
Simon Vaucher <s.vaucher@applicaster.com>
2016-10-05 12:39:24 +0300

    update to react-native 0.34 notation for activity result

--- android/src/main/java/com/applicaster/RNYoutubePlayer/YoutubePlayerModule.java
+++ android/src/main/java/com/applicaster/RNYoutubePlayer/YoutubePlayerModule.java
@@ -61,5 +61,14 @@ public class YoutubePlayerModule extends ReactContextBaseJavaModule implements A
             promise.resolve(map);
         }
     }
-}
 
+    public void onActivityResult(Activity activity, int requestCode, int resultCode, Intent data) {
+        Log.d(TAG, "on activity result");
+        if (requestCode == YOUTUBE_PLAYER_ACTIVITY && resultCode == Activity.RESULT_CANCELED) {
+            Log.d(TAG, "promise resolved");
+            WritableMap map = Arguments.createMap();
+            map.putString("state", STATE_STOPPED);
+            promise.resolve(map);
+        }
+    }
+}
